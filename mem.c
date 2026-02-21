564f63501b9a75468b88d6f4f99887984c169f52
leowercase <126769796+leowercase@users.noreply.github.com>
2026-02-21 23:16:18 +0200

    changes to data structures

--- mem.c
+++ mem.c
@@ -18,13 +18,3 @@ size_t grow_cap(size_t cap)
   return cap < 8 ? 8 : (cap * 2);
 }
 
-void adjust_array_cap(void **array, const size_t elem_size,
-    size_t *cap, size_t required_cap)
-{
-  if (*cap < required_cap) {
-    size_t new_cap = grow_cap(*cap);
-
-    *array = allocate(*array, new_cap * elem_size);
-    *cap = new_cap;
-  }
-}
--- mem.h
+++ mem.h
@@ -3,12 +3,12 @@
 
 #include "util.h"
 
+// Helpers for dealing with memory.
+
+// Allocate space, or reallocate existing ptr
 void *allocate(void *ptr, size_t size);
 
-// Grows capacity, guaranteed to be base of 2
+// Grow capacity; guaranteed to be base of 2
 size_t grow_cap(size_t cap);
-
-void adjust_array_cap(void **array, const size_t elem_size,
-    size_t *cap, size_t required_cap);
 
 #endif
