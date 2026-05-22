b6d7c70e17a3bc8844abbb8434d666374312439b
leowercase <126769796+leowercase@users.noreply.github.com>
2026-05-22 14:35:01 +0300

    add ANSI color output

--- web/pre.js
+++ web/pre.js
@@ -1,8 +1,12 @@
 // https://emscripten.org/docs/tools_reference/emcc.html#emcc-pre-js
+
+const { AnsiUp } = await import("/ansi_up/ansi_up.js", { with: { type: "js" } });
 {
+  const ansi_up = new AnsiUp();
+
   const stdout = document.getElementById("stdout");
-  const print =
-    text => stdout.insertAdjacentHTML("beforeend", text + "\n");
+  const print = text =>
+    stdout.insertAdjacentHTML("beforeend", ansi_up.ansi_to_html(text + "\n"));
 
   Module["print"] = Module["printErr"] = print;
 }
