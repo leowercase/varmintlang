d39051f8e09f3ecdad499e301ee89fcf06670390
leowercase <126769796+leowercase@users.noreply.github.com>
2026-05-24 11:32:50 +0300

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let
-  pkgs = import <nixpkgs> {};
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/3d8f0f3f72a6cd4d93d0ad13203f2ea1cb7e1456.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 {
   varmint = pkgs.callPackage ./varmint.nix {};
